f6255e81b69c1df5ec41f651b56ccc928b99fbbe
guchi3f4 <guchi3f4@gmail.com>
2021-07-23 06:09:49 +0000

    3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      @book = book
+      render "index"
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      @books = Book.all
+      @book = book
+      render "index"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
+  end
+end
+
+private
+  def book_params
+    params.require(:book).permit(:book, :title, :body)
+  end
